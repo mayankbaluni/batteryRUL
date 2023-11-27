429ca3e74d43b25552f83781ac0a893729779ba2
mayank baluni <mayankbaluni@gmail.com>
2023-11-27 13:23:03 +0530

    create and train model

--- bat.py
+++ bat.py
@@ -3,5 +3,19 @@ from sklearn.model_selection import train_test_split
 
 # Load the dataset
 df = pd.read_csv('path_to_your_dataset.csv')
+print(df.describe())
+X = df.drop('target_column', axis=1) # Replace 'target_column' with your RUL column
+y = df['target_column']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import GridSearchCV
+
+# Model selection
+model = RandomForestRegressor()
+
+model.fit(X_train, y_train)
+
 
 
